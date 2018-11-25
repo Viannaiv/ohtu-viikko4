13358a9c7de2b9fe5c2bcab1bde164330702c0bb
Viannaiv <viva95@windowslive.com>
2018-11-25 18:38:02 +0200

    Exercise 5 completed

--- Verkkokauppa1/src/test/java/ohtu/kauppa/KauppaTest.java
+++ Verkkokauppa1/src/test/java/ohtu/kauppa/KauppaTest.java
@@ -14,43 +14,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-/**
- *
- * @author Vivianna
- */
+
 public class KauppaTest {
-    
-    public KauppaTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    Kauppa k;
     
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+        k = new Kauppa(varasto, pankki, viite);
     }
     
     @Test
-    public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeinYhdellaTuoteella() {
-        Pankki pankki = mock(Pankki.class);
-        
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);   
+    public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeinYhdellaTuoteella() { 
         when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
+        
         when(varasto.saldo(1)).thenReturn(10); 
-        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));          
 
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
@@ -61,18 +45,12 @@ public class KauppaTest {
     
     @Test
     public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeinUseallaTuotteella() {
-        Pankki pankki = mock(Pankki.class);
-        
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);   
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.saldo(2)).thenReturn(6);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 7));
-
-        Kauppa k = new Kauppa(varasto, pankki, viite);
 
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
@@ -83,17 +61,11 @@ public class KauppaTest {
     }
     
     @Test
-    public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeinUseallaSamallaTuotteella() {
-        Pankki pankki = mock(Pankki.class);
-        
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);   
+    public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeinUseallaSamallaTuotteella() { 
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(6);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "juusto", 7));
-
-        Kauppa k = new Kauppa(varasto, pankki, viite);
 
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
@@ -105,18 +77,12 @@ public class KauppaTest {
     
     @Test
     public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeinKahdellaTuotteellaJoistaToistaEiOleVarastossa() {
-        Pankki pankki = mock(Pankki.class);
-        
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(0);
         when(varasto.saldo(2)).thenReturn(6);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 7));
-
-        Kauppa k = new Kauppa(varasto, pankki, viite);
 
         k.aloitaAsiointi();
         k.lisaaKoriin(1);
@@ -124,6 +90,59 @@ public class KauppaTest {
         k.tilimaksu("pekka", "12345");
 
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(7));   
+    }
+    
+    @Test
+    public void asioinninAloitusNollaaVanhatOstoksetJaUusienOstostenTilisiirronSummaOnOikea() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(6);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 7));
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(7));   
+    }
+    
+    @Test
+    public void kauppaPyytaaUudenViitenumeronJokaiselleTapahtumalle() {
+        when(varasto.saldo(2)).thenReturn(6);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 7));
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        
+        verify(viite, times(3)).uusi();
+    }
+    
+    @Test
+    public void poistaKoristaPoistaaTuotteenNiinEttaOstoksetPaattavanTilisiirronSummaOnOikea() {
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(6);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 7));
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(7));   
     }
 
 
